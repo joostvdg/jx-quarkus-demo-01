3c045b08e83f0777815c42e05194bad290fdcdd6
Joost van der Griendt <joostvdg@gmail.com>
2020-05-12 17:34:00 +0200

    forgot to set autoincrement for id column
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

--- src/main/resources/db/migration/V1.0.0__initial.sql
+++ src/main/resources/db/migration/V1.0.0__initial.sql
@@ -33,7 +33,7 @@ DROP TABLE IF EXISTS `fruit`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fruit` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `color` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
