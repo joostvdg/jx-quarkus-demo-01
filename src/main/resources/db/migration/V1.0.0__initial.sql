81cb8a32998147c93242a4dce22f01b266df9d06
Joost van der Griendt <joostvdg@gmail.com>
2020-05-12 18:10:04 +0200

    commented out sql code still executed
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

--- src/main/resources/db/migration/V1.0.0__initial.sql
+++ src/main/resources/db/migration/V1.0.0__initial.sql
@@ -40,50 +40,5 @@ CREATE TABLE `fruit` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE UNIQUE INDEX fruit_name_color ON fruit(color,name);
+CREATE UNIQUE INDEX `fruit_name_color` ON `fruit` (`name`, `color`);
 
---
--- Dumping data for table `Fruit`
---
---
--- LOCK TABLES `Fruit` WRITE;
--- /*!40000 ALTER TABLE `Fruit` DISABLE KEYS */;
--- /*!40000 ALTER TABLE `Fruit` ENABLE KEYS */;
--- UNLOCK TABLES;
-
---
--- Table structure for table `hibernate_sequence`
---
-
--- DROP TABLE IF EXISTS `hibernate_sequence`;
--- /*!40101 SET @saved_cs_client     = @@character_set_client */;
--- /*!40101 SET character_set_client = utf8 */;
--- CREATE TABLE `hibernate_sequence` (
---   `next_val` bigint(20) DEFAULT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- /*!40101 SET character_set_client = @saved_cs_client */;
---
--- --
--- -- Dumping data for table `hibernate_sequence`
--- --
---
--- -- LOCK TABLES `hibernate_sequence` WRITE;
--- /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
--- INSERT INTO `hibernate_sequence` VALUES (1);
--- INSERT INTO `hibernate_sequence` VALUES (2);
--- INSERT INTO `hibernate_sequence` VALUES (3);
--- INSERT INTO `hibernate_sequence` VALUES (4);
--- INSERT INTO `hibernate_sequence` VALUES (5);
-/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
--- UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-05-10 18:08:37
